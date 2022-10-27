87869fd4653bf2637b381da561909d33937fa9b2
Uncanny7043 <ejanred3@gmail.com>
2022-10-27 09:14:28 +0800

    Added `games` command, updated README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# flchans4world-bot
+# flchans4world-bot [WIP]
+
+A Discord bot that fetches and prints data from [aoe4world's API](https://aoe4world.com/)
+
+For now the bot is only able to fetch 1v1 Ladder user data
+
+## Planned Features
+    * Find a way to fetch players' Hidden ELO ratings
+    * Add more commands
+    * Add support for teamgames stats
+
+## API used
+    * https://aoe4world.com/api 
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import os
+
 import discord
 import requests
-import os
+from discord.ext import commands
 from dotenv import load_dotenv
 
+"""
+TODO:
+    * Find a way to fetch ELO ratings from aoe4world API
+    * Add commands
+"""
 response = requests.get("https://aoe4world.com/api")
 print(f'Connection status code: {response.status_code}')
 
@@ -10,10 +17,27 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+intents.message_content = True
 
-@client.event
+bot = commands.Bot(command_prefix='$',intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord')
+    print(f'{bot.user} has connected to Discord')
 
-client.run(TOKEN)
+@bot.command()
+async def elo(ctx):
+    await ctx.send("Your ELO is 1180!")
+
+@bot.command()
+async def games(ctx, ign: str):
+    parameters = {"query": ign}
+    user_profile = requests.get("https://aoe4world.com/api/v0/leaderboards/rm_solo", params=parameters)
+
+    games_count = user_profile.json()['players'][0]['games_count']
+    wins_count = user_profile.json()['players'][0]['wins_count']
+    win_rate = user_profile.json()['players'][0]['win_rate']
+
+    await ctx.send(f"Games record in 1v1 RM for: {ign}\nGames Count: {games_count}\nWins Count: {wins_count}\nWin Rate: {win_rate}")
+
+bot.run(TOKEN)
