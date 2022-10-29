dde17c057ed175bb3c6b196cdbe5116fcf5bef6f
Uncanny7043 <ejanred3@gmail.com>
2022-10-29 12:11:26 +0800

    Formatted `leaderboard` function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
+import time
+
+from os import getenv
 from dotenv import load_dotenv
 
 import requests
@@ -11,13 +13,14 @@ from discord.ext import commands
         * Find a way to fetch ELO ratings from aoe4world API
         * Add commands
             * Finish implementing `display top 10 players in rm_solo leaderboards`
+                * Add more player information
 """
 
 response = requests.get("https://aoe4world.com/api")
 print(f'Connection status code: {response.status_code}')
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -51,17 +54,34 @@ async def games(ctx, ign: str):
     wins_count = user_profile.json()['players'][0]['wins_count']
     win_rate = user_profile.json()['players'][0]['win_rate']
 
-    await ctx.send(f"Games record in 1v1 RM for: {ign}\nGames Count: {games_count}\nWins Count: {wins_count}\nWin Rate: {win_rate}")
+    #await ctx.send(f"Games record in 1v1 RM for: {ign}\nGames Count: {games_count}\nWins Count: {wins_count}\nWin Rate: {win_rate}")
+
+    embed = discord.Embed(title="Match record for:", description=f"{ign}")
+    embed.add_field(name="Games Count:", value=games_count, inline=False)
+    embed.add_field(name="Wins Count:", value=wins_count, inline=False)
+    embed.add_field(name="Win Rate:", value=f"{win_rate}%", inline=False)
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def leaderboard(ctx): 
     '''
-    INCOMPLETE
+    INCOMPLETE: add more player info
     '''
     board = requests.get("https://aoe4world.com/api/v0/leaderboards/rm_solo")
 
-    await ctx.send("Top 10 in 1v1 Ladder")
+    t = time.strftime("%m/%d/%Y %H:%M")
+
+    embed = discord.Embed(title="Top 10 in 1v1 Ladder", description=f"`as of {t}`")
     for i in range(10):
-        await ctx.send(f"{i+1}: {board.json()['players'][i]['name']}")
+        embed.add_field(name=f"{i+1}. {board.json()['players'][i]['name']}", value="placeholder", inline=False)
+    
+    await ctx.send(embed=embed)
+    
+@bot.command()
+async def test(ctx):
+    embed = discord.Embed(title="Fuck you", description="Ottoids suck") #,color=Hex code
+    embed.add_field(name="Name", value="you can make as much as fields you like to")
+    embed.set_footer(text="footer") #if you like to
+    await ctx.send(embed=embed)
 
 bot.run(TOKEN)
